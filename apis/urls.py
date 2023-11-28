a42ee2f9bf0282fa0abf522f873d9e7329a3a1af
SteffRhes <stefanresch@protonmail.com>
2023-11-28 17:36:59 +0100

    lint apis/urls.py

--- apis/urls.py
+++ apis/urls.py
@@ -1,20 +1,17 @@
 from django.conf.urls import url, include, handler404
 from django.contrib import admin
-from django.conf import settings
-from rest_framework import routers
 from django.urls import path
 
 from apis_core.apis_entities.api_views import GetEntityGeneric
 
 urlpatterns = [
-    url(r'^apis/', include('apis_core.urls', namespace="apis")),
-    url(r'^bibsonomy/', include('apis_bibsonomy.urls', namespace="bibsonomy")),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^bibsonomy/', include('apis_bibsonomy.urls', namespace="bibsonomy")),
+    url(r"^apis/", include("apis_core.urls", namespace="apis")),
+    url(r"^bibsonomy/", include("apis_bibsonomy.urls", namespace="bibsonomy")),
+    url(r"^api-auth/", include("rest_framework.urls", namespace="rest_framework")),
     path(r"entity/<int:pk>/", GetEntityGeneric.as_view(), name="GetEntityGenericRoot"),
-    url(r'^admin/', admin.site.urls),
-    url(r'^', include('webpage.urls', namespace='webpage')),
+    url(r"^admin/", admin.site.urls),
+    url(r"^", include("webpage.urls", namespace="webpage")),
 ]
 
 
-handler404 = 'webpage.views.handler404'
+handler404 = "webpage.views.handler404"
